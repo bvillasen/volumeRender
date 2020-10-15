d6dd3f78d98eef463250ef824925210a4666b29b
bruno <bvillasen@gmail.com>
2020-10-15 13:40:52 -0700

    new

--- prepare_data.py
+++ prepare_data.py
@@ -39,9 +39,9 @@ data_vals = data[data_type][field]
 data_vals -= data_vals.min()
 
 # Normalize Data
-max_val = data_vals.max() 
+max_val = data_vals.max() / 10 
+data_vals = np.clip( data_vals, a_min=None, a_max=max_val ) 
 data_vals = np.log10(data_vals + 1) / np.log10( max_val + 1)
-# data_vals = np.clip( data_vals, a_min=None, a_max=max_val ) 
 
 # Change to 256 range
 data_vals = (255*(data_vals)).astype(np.uint8)
