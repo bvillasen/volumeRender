da5fb138f5998eb166a7b296e352fe7df3f30bee
bruno <bvillasen@gmail.com>
2020-10-15 11:36:09 -0700

    new

--- data_src/tools.py
+++ data_src/tools.py
@@ -13,7 +13,7 @@ def create_directory( dir ):
   if dirName in dir_list: print( " Directory exists" )
   else:
     os.mkdir( dir )
-    print " Directory created"
+    print( " Directory created" )
 
 def get_files_names( fileKey, inDir, type='cholla' ):
   if type=='nyx': dataFiles = [f for f in listdir(inDir) if (f.find(fileKey) >= 0 )  ]
