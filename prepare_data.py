4bb14345eb2b4ae50349600fc0547732b0e0dce2
bruno <bvillasen@gmail.com>
2020-10-15 13:47:09 -0700

    new

--- prepare_data.py
+++ prepare_data.py
@@ -39,7 +39,7 @@ data_vals = data[data_type][field]
 data_vals -= data_vals.min()
 
 # Normalize Data
-max_val = data_vals.max() / 10 
+max_val = data_vals.max() / 100 
 data_vals = np.clip( data_vals, a_min=None, a_max=max_val ) 
 data_vals = np.log10(data_vals + 1) / np.log10( max_val + 1)
 
