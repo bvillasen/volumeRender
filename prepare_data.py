b38eb469e3229c3b50f804f2185e8a62f191eded
bruno <bvillasen@gmail.com>
2020-10-17 19:01:47 -0700

    new

--- prepare_data.py
+++ prepare_data.py
@@ -15,53 +15,58 @@ nPoints = 1024
 # dataDir = '/raid/bruno/data/'
 dataDir = '/data/groups/comp-astro/bruno/'
 inDir = dataDir + 'cosmo_sims/{0}_hydro_50Mpc/output_files_pchw18/'.format(nPoints)
+stats_dir = inDir + 'statistics/'
 outDir = dataDir + 'cosmo_sims/{0}_hydro_50Mpc/snapshots_prepared/'.format(nPoints)
 create_directory( outDir )
 
+data_type = 'hydro'
+# data_type = 'particles'
 
-n_snapshot = 169
-for n_snapshot in range(170):
+# Load Statistics
+statistics = h5.File( stats_dir + 'stats_{0}.h5'.format(data_type))
 
-  data_type = 'hydro'
-  # data_type = 'particles'
+fields = ['density']
+precision = np.float32
 
-  fields = ['density']
+Lbox = 5000    #kpc/h
+if nPoints == 1024: proc_grid = [ 4, 2, 2]
+if nPoints == 2048: proc_grid = [ 8, 8, 8]
+box_size = [ Lbox, Lbox, Lbox ]
+grid_size = [ nPoints, nPoints, nPoints ] #Size of the simulation grid
+subgrid = [ [0, nPoints], [0, nPoints], [0, nPoints] ] #Size of the volume to load
 
-  precision = np.float32
+field = 'density'
 
-  Lbox = 5000    #kpc/h
-  if nPoints == 1024: proc_grid = [ 4, 2, 2]
-  if nPoints == 2048: proc_grid = [ 8, 8, 8]
-  box_size = [ Lbox, Lbox, Lbox ]
-  grid_size = [ nPoints, nPoints, nPoints ] #Size of the simulation grid
-  subgrid = [ [0, nPoints], [0, nPoints], [0, nPoints] ] #Size of the volume to load
-  data = load_snapshot_data_distributed( n_snapshot, inDir, data_type, fields, subgrid,  precision, proc_grid,  box_size, grid_size, show_progess=True )
-
-  field = 'density'
-
-  min_val = 0.020682
-  max_val = 6375.8875
-
-  data_vals = data[data_type][field]  
-  # min_val = data_vals.min()
-  data_vals -= min_val
+min_val = statistics[field].attrs['min_global']
+max_val = statistics[field].attrs['max_global']
+print( "Min: {0}   Max: {1}".format(min_val, max_val ))
 
 
-  # Normalize Data
-  # max_val = data_vals.max()
-  max_val = max_val / 1000 
-  data_vals = np.clip( data_vals, a_min=None, a_max=max_val ) 
-  data_vals = np.log10(data_vals + 1) / np.log10( max_val + 1)
+
+# for n_snapshot in range(170):
+
+  # data = load_snapshot_data_distributed( n_snapshot, inDir, data_type, fields, subgrid,  precision, proc_grid,  box_size, grid_size, show_progess=True )
 
 
-  # print( "Min: {0}   Max: {1}".format(min_val, max_val ))
-
-  # Change to 256 range
-  data_vals = (255*(data_vals)).astype(np.uint8)
-
-  #Write to file
-  out_file_name = outDir + '{0}_{1}_{2}.h5'.format( data_type, field, n_snapshot )
-  out_file = h5.File( out_file_name, 'w')
-  out_file.create_dataset( field, data=data_vals )
-  out_file.close()
-  print( "Saved File: " + out_file_name )
+  # data_vals = data[data_type][field]  
+  # # min_val = data_vals.min()
+  # data_vals -= min_val
+  # 
+  # 
+  # # Normalize Data
+  # # max_val = data_vals.max()
+  # max_val = max_val / 1000 
+  # data_vals = np.clip( data_vals, a_min=None, a_max=max_val ) 
+  # data_vals = np.log10(data_vals + 1) / np.log10( max_val + 1)
+  # 
+  # 
+  # 
+  # # Change to 256 range
+  # data_vals = (255*(data_vals)).astype(np.uint8)
+  # 
+  # #Write to file
+  # out_file_name = outDir + '{0}_{1}_{2}.h5'.format( data_type, field, n_snapshot )
+  # out_file = h5.File( out_file_name, 'w')
+  # out_file.create_dataset( field, data=data_vals )
+  # out_file.close()
+  # print( "Saved File: " + out_file_name )
